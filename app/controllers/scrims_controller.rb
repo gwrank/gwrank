80c43e4b27acdf77b019a15ba9de43fbc449a99e
Ko Arkalon <ko.arkalon@gmail.com>
2021-03-18 01:45:07 +0100

    Ask to Log in with Discord to see the current scrimers list

--- app/controllers/scrims_controller.rb
+++ app/controllers/scrims_controller.rb
@@ -1,4 +1,6 @@
 class ScrimsController < ApplicationController
+  before_action :authenticate_player!
+
   def index
     @current_registrations = Registration.current_registrations.order(registered_at: :asc)
     @current_scrims = Scrim.current_scrims
