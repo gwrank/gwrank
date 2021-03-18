a100a36a05682a4cc2cf189e655b54e170eec207
Ko Arkalon <ko.arkalon@gmail.com>
2021-03-18 19:26:17 +0100

    Improve players invitations

--- app/controllers/administration/players/invitations_controller.rb
+++ app/controllers/administration/players/invitations_controller.rb
@@ -3,7 +3,7 @@ module Administration
     def create
       @player = Player.friendly.find(params[:player_id])
       bot = Discordrb::Bot.new token: ENV['DISCORD_BOT_TOKEN']
-      bot.user(@player.uid).pm('Hello! Are you free for scrim? Register on https://gwrank.com')
+      bot.user(@player.uid).pm('Hello! Are you free for scrim? !register in #scrims channel of GWRank.com Discord. Here is a direct link to the channel: https://discord.com/channels/788820055029973045/806189775308193802')
       redirect_to administration_players_path
     end
   end
