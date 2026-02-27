20c557fac23ea2b4db9daefeb60fbad8fad7d6e8
Arka6594 <ko.arkalon@gmail.com>
2026-02-27 15:10:13 +0100

    fix: homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @tournaments = Tournament.last(2)
+    @tournaments = Tournament.order(date: :desc).first(2)
+    @matches = Match.order(played_at: :desc).first(1)
   end
 end
