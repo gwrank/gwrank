c0cebdea1adb189e3f6d1a3c174e43d3aa283e76
Arka6594 <ko.arkalon@gmail.com>
2026-03-06 20:51:30 +0100

    feat: ELO homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @tournaments = Tournament.order(date: :desc).first(2)
-    @matches = Match.order(played_at: :desc).first(1)
+    # ELO statistics
+    @top_elo_players = Player.joins(:team_players)
+      .where.not(elo_rating: nil)
+      .merge(TeamPlayer.joins(:team).merge(Team.where.not(match_id: nil)))
+      .group('players.id', 'players.igname')
+      .order('players.elo_rating DESC')
+      .first(3)
   end
 end
