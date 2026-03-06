5101d7a37647611d39e573295dfee433bd109186
Arka6594 <ko.arkalon@gmail.com>
2026-03-06 23:39:00 +0100

    feat: remove pagination limits

--- config/initializers/pagy.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-Pagy::OPTIONS[:limit] = 10               # Limit the items per page
-Pagy::OPTIONS[:client_max_limit] = 10    # The client can request a limit up to 100
-Pagy::OPTIONS.freeze
