f708805f361def62589c9d56987acce0123e9aba
Arka6594 <ko.arkalon@gmail.com>
2026-03-06 13:36:57 +0100

    feat: pagination limit

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,6 @@
 class PlayersController < ApplicationController
   def index
-    @pagy, @players = pagy(Player.with_igname.order(updated_at: :desc, created_at: :desc))
+    @pagy, @players = pagy(Player.with_igname.order(updated_at: :desc, created_at: :desc), limit: 18)
   end
 
   def show
