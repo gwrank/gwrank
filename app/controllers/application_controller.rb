7856bf60b5e3e8fdcc4ed6dc1a25e49b3fbb9be2
Arka6594 <ko.arkalon@gmail.com>
2026-03-18 09:22:50 +0100

    fix: after sign in path to origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def after_sign_in_path_for(player)
+    request.env["omniauth.origin"] || stored_location_for(player) || root_path
+  end
+
   def pundit_user
     current_player
   end
