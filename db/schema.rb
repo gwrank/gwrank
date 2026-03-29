e7b8154a3f38676b4e72c14713e5da23095cbe82
Arka6594 <ko.arkalon@gmail.com>
2026-03-30 00:36:16 +0200

    fix: gwrank cache db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_15_185159) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_29_223521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -304,6 +304,17 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_15_185159) do
     t.integer "template_skill_id"
     t.datetime "updated_at", null: false
     t.index ["profession_id"], name: "index_skills_on_profession_id"
+  end
+
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.integer "byte_size", null: false
+    t.datetime "created_at", null: false
+    t.binary "key", null: false
+    t.bigint "key_hash", null: false
+    t.binary "value", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "team_player_skills", force: :cascade do |t|
