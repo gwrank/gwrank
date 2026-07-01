e2fc187068d013f6a0799bbd0deda8ce599fb519
Arka6594 <ko.arkalon@gmail.com>
2026-07-01 23:42:12 +0200

    fix

--- app/controllers/scrims_controller.rb
+++ app/controllers/scrims_controller.rb
@@ -1,6 +1,4 @@
 class ScrimsController < ApplicationController
-  before_action :authenticate_player!
-
   def index
     @current_registrations = Registration.current_registrations.order(registered_at: :asc)
     @current_scrims = Scrim.current_scrims
