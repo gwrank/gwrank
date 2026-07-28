c8414f7fd8e0f00b62530785b68a1d55093e64a1
Arka.6594 <ko.arkalon@gmail.com>
2026-07-28 19:54:28 +0200

    fix: update invitation message to use /scrim panel

--- app/controllers/administration/players/invitations_controller.rb
+++ app/controllers/administration/players/invitations_controller.rb
@@ -3,7 +3,7 @@ module Administration
     def create
       @player = Player.friendly.find(params[:player_id])
       bot = Discordrb::Bot.new token: ENV['DISCORD_BOT_TOKEN']
-      bot.user(@player.uid).pm('Hello! Are you free for scrim? Register in #scrims channel of GWRank.com Discord using /queue open. Here is a direct link to the channel: https://discord.com/channels/788820055029973045/806189775308193802')
+       bot.user(@player.uid).pm('Hello! Are you free for scrim? Register in #scrims channel of GWRank.com Discord using /scrim panel. Here is a direct link to the channel: https://discord.com/channels/788820055029973045/806189775308193802')
       redirect_to administration_players_path
     end
   end
