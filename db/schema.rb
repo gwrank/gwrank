228f6e0a4f77d3ee38b8925c8493d30abb180510
Arka.6594 <ko.arkalon@gmail.com>
2026-08-22 00:34:34 +0200

    feat: move action cable to solid_cable on primary database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_21_220843) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_21_223128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -322,6 +322,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_21_220843) do
     t.integer "template_skill_id"
     t.datetime "updated_at", null: false
     t.index ["profession_id"], name: "index_skills_on_profession_id"
+  end
+
+  create_table "solid_cable_messages", force: :cascade do |t|
+    t.binary "channel", null: false
+    t.bigint "channel_hash", null: false
+    t.datetime "created_at", null: false
+    t.binary "payload", null: false
+    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
+    t.index ["channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
+    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
   end
 
   create_table "solid_cache_entries", force: :cascade do |t|
