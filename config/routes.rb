4e846cf4ac422a15faa4449363c31eaef04d581b
Arka.6594 <ko.arkalon@gmail.com>
2026-08-24 15:32:42 +0200

    chore(dev): serve swagger ui for the teambuilds api in development

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount Rswag::Api::Engine => '/api-docs'
+    mount Rswag::Ui::Engine => '/api-docs'
+  end
   namespace :api do
     namespace :v1 do
       resources :items, only: [:index, :show]
