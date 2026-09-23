b3fc387fca393c594e1fea85b8422173a614dd2a
Ko Arkalon <ko.arkalon@gmail.com>
2026-09-23 10:58:19 +0200

    test: expand shared room endpoint coverage

--- test/controllers/api/v1/rooms_controller_test.rb
+++ test/controllers/api/v1/rooms_controller_test.rb
@@ -4,6 +4,7 @@ module Api::V1
   class RoomsControllerTest < ActionDispatch::IntegrationTest
     test "creates a room for an API token" do
       player = create_player
+      requested_at = Time.current
 
       post api_v1_rooms_path, headers: auth_headers(player)
 
@@ -12,6 +13,7 @@ module Api::V1
       assert_match(/\A[A-Z2-9]{4}-[A-Z2-9]{3}\z/, body.fetch("code"))
       assert_match(/\A[A-Za-z0-9_-]+\z/, body.fetch("creatorSecret"))
       assert_match(%r{\Awss://.*?/cable\z}, body.fetch("websocketUrl"))
+      assert_in_delta requested_at + 2.hours, Time.iso8601(body.fetch("expiresAt")), 5.seconds
       assert_equal 8, body.dig("limits", "participants")
       assert_equal 16_384, body.dig("limits", "payloadBytes")
       assert_equal 10, body.dig("limits", "messagesPerSecond")
@@ -21,6 +23,34 @@ module Api::V1
       post api_v1_rooms_path
 
       assert_response :unauthorized
+    end
+
+    test "rejects an invalid API token" do
+      post api_v1_rooms_path, headers: { "Authorization" => "Bearer invalid-token" }
+
+      assert_response :unauthorized
+    end
+
+    test "returns rooms_full when the store has reached capacity" do
+      player = create_player
+      full_store = Class.new do
+        def create!
+          raise Rooms::SessionStore::Error.new(close_code: 503, reason: "rooms_full")
+        end
+      end.new
+
+      Rooms::SessionStore.stub(:new, full_store) do
+        post api_v1_rooms_path, headers: auth_headers(player)
+      end
+
+      assert_response :service_unavailable
+      assert_equal({
+        "errors" => [{
+          "code" => "rooms_full",
+          "message" => "Aucune place de salon disponible"
+        }]
+      }, response.parsed_body)
+      assert_not_includes response.parsed_body, "creatorSecret"
     end
   end
 end
